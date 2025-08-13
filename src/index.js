9b6e2850ec8abb2dfcce01cb8809676d89606dec
Hlengiwe-Mthimkhulu <hlengiwenorah29@gmail.com>
2025-08-13 20:26:02 +0200

    added the basic javaScript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateVerse(event) {
+  event.preventDefault();
 
+  new Typewriter("#mood", {
+    strings: ["Trust in the lord and lean not in your understanding"],
+    autoStart: true,
+    delay: 100,
+    cursor: "",
+    loop: true,
+  });
+}
+
+let moodFormElement = document.querySelector("#form-generator");
+moodFormElement.addEventListener("submit", generateVerse);
